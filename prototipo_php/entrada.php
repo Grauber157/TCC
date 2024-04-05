678d250511f8345722806f325753e630016ddc21
Gabriel Faria Dias <faria.dias@aluno.ifsp.edu.br>
2024-04-05 09:36:16 -0300

    atuaizado

--- prototipo_php/entrada.php
+++ prototipo_php/entrada.php
@@ -10,46 +10,49 @@
     <h1>
         <p>
             <?php 
+                //Gera os valores e o resultado da questão
                 $n1 = rand(1, 10);
                 $n2 = rand(1, 10);
                 $resul = $n1 + $n2;
                 
-                for ($i=0; $i < 3; $i++) 
+                //Estrutura de Repetição For() para randomizar as questões
+                //$alternativas[0] é o resultado correto
+                $alternativas[0] = $resul;
+                for ($i=1; $i < 3; $i++) 
                 { 
-                    
+                    $alternativas[$i] = $resul - rand
                 }
 
                 $escolha = 0;
-                
+                //h1 para escrever o codígo
                 echo "{$n1} + {$n2} = X";
-
-
             ?>
         </p>
     </h1>
 
+    <!--ALTERNATIVAS-->
     <form action="exe1.php" method="get">
         <button type="submit" name="escolha">
             <?php 
-                echo $resul;
+                echo $alternativas[0];
                 $escolha = 1;
             ?>
         </button>
         <button type="submit" name="escolha">
             <?php 
-                echo $resul-5;
+                echo $alternativas[1];
                 $escolha = 2;
             ?>
         </button>
         <button type="submit" name="escolha">
             <?php 
-                echo $resul-1;
+                echo $alternativas[2];
                 $escolha = 3;
             ?>
         </button>
         <button type="submit" name="escolha">
             <?php 
-                echo $resul+4;
+                echo $alternativas[3];
                 $escolha = 4;
             ?>
         </button>
