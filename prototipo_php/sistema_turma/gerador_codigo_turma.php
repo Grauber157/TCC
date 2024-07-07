f923d4702cc995a6da28c2a65f863e19dc3c4f97
Gabriel Faria Dias <faria.dias@aluno.ifsp.edu.br>
2024-07-07 12:25:39 -0300

    Sistema Turma

--- prototipo_php/sistema_turma/gerador_codigo_turma.php
+++ prototipo_php/sistema_turma/gerador_codigo_turma.php
@@ -1,6 +1,6 @@
 <?php
-    //funcao 'chr()' para transformar numeros em 
-    //string pela tabela ASCII
+    #funcao 'chr()' para transformar numeros em 
+    #string pela tabela ASCII
     $letra1 = chr(rand(65, 90));
     $letra2 = chr(rand(65, 90));
     
@@ -12,7 +12,7 @@
     $codigo[2] = $numero1;
     $codigo[3] = $numero2;
     
-    //implode para juntar as letras e numeros em uma só string
+    #implode para juntar as letras e numeros em uma só string
     $codigo_turma = implode("", $codigo);
     
     echo $codigo_turma;
