42674da3d0ba56af54f995d5b3d362d6059fd118
Gabriel Faria Dias <faria.dias@aluno.ifsp.edu.br>
2024-07-08 11:50:49 -0300

    Sistema de Turma

--- prototipo_php/sistema_turma/entrar_turma.php
+++ prototipo_php/sistema_turma/entrar_turma.php
@@ -1,5 +1,13 @@
 <?php
-    ////REMOVER////
+    session_start();
+    $usuario = $_SESSION["login"];
+
+
+    $codigo_turma = $_POST["codigo_turma"];
+    $senha = $_POST["senha"];
+
+    ////REMOVER (substituir pelo 'include()')////
+
     #Parâmetros para realizar a conexão com o banco de dados
     $hostname = "localhost";
     $username = "root";
@@ -15,5 +23,32 @@
     echo "Conexão bem sucedida\n";   
     ////REMOVER////
     
-    
+    $stmt = mysqli_prepare($link, "SELECT codigo, senha_turma, nome_turma FROM turma WHERE codigo = ? and senha_turma = ?");
+
+    if($stmt)
+    {
+        mysqli_stmt_bind_param($stmt, "ss", $codigo_turma, $senha);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $codigo_banco, $senha_banco, $nome_turma);
+        mysqli_stmt_fetch($stmt);
+
+        if($codigo_turma == $codigo_banco and $senha == $senha_banco)
+        {
+            $stmt2 = mysqli_prepare($link, "INSERT INTO usuario(codigo_turma) VALUES($codigo_banco)");
+            mysqli_stmt_execute($stmt2);
+            mysqli_stmt_fetch($stmt2);
+
+            echo "Sucesso ao entrar na turma!<br>";
+            echo "Bem vindo a Turma $nome_turma!<br>";
+
+            mysqli_stmt_close($stmt2);
+        }
+        else
+        {
+            echo "Erro ao entrar na turma!<br>";
+        }
+        mysqli_stmt_close($stmt);
+    }
+
+
 ?>
