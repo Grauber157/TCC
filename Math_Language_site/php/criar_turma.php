be9dfff212f65db73888b4310238a749194a9dc2
Grauber157 <gabrielfariadias16@gmail.com>
2024-09-09 11:55:36 -0300

    termina em casa

--- Math_Language_site/php/criar_turma.php
+++ Math_Language_site/php/criar_turma.php
@@ -1,12 +1,24 @@
-<?php
-    session_start();
 
-    include('../php/include/conexao.php');
-    include('../php/include/gerador_codigo_turma.php');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //session_start();
+        include('../php/include/gerador_codigo_turma.php');
+        echo $codigo_turma;
+        //include('../php/include/conexao.php');
+        
 
-    $nome_turma = $_POST["nome"];
-    $senha_turma = $_POST["senha"];
+        $nome_turma = $_POST["nome"];
+        $senha_turma = $_POST["senha"];
 
-    echo $codigo_turma;
-    
-?>
+        
+        
+    ?>    
+</body>
+</html>
