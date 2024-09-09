4355bcfc9b59705ed5b9ca81217952fd042ef9c1
Grauber157 <gabrielfariadias16@gmail.com>
2024-09-09 11:43:57 -0300

    turma

---
+++ Math_Language_site/php/criar_turma.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+
+    include('');
+
+    $nome_turma = $_POST["nome"];
+    $senha_turma = $_POST["senha"];
+
+    
+?>
--- Math_Language_site/php/include/gerador_codigo_turma.php
+++ Math_Language_site/php/include/gerador_codigo_turma.php
@@ -1,17 +1,25 @@
 <?php
     #funcao 'chr()' para transformar numeros em 
     #string pela tabela ASCII
-    $letra1 = chr(rand(65, 90));
-    $letra2 = chr(rand(65, 90));
+    $end = true;
+    while($end)
+    {
+        #funcao 'chr()' para transformar numeros em 
+        #string pela tabela ASCII
+        $letra1 = chr(rand(65, 90));
+        $letra2 = chr(rand(65, 90));
+        
+        $numero1 = rand(0, 9);
+        $numero2 = rand(0, 9);
+        
+        $codigo[0] = $letra1;
+        $codigo[1] = $letra2;
+        $codigo[2] = $numero1;
+        $codigo[3] = $numero2;
+        
+        #implode para juntar as letras e numeros em uma só string
+        $codigo_turma = implode("", $codigo);
+
+    }
     
-    $numero1 = rand(0, 9);
-    $numero2 = rand(0, 9);
-    
-    $codigo[0] = $letra1;
-    $codigo[1] = $letra2;
-    $codigo[2] = $numero1;
-    $codigo[3] = $numero2;
-    
-    #implode para juntar as letras e numeros em uma só string
-    $codigo_turma = implode("", $codigo);
 ?>
