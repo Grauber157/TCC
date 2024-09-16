7b7bc8208f5508f3082e293aedb620c7f0045f43
Gabriel Faria Dias <faria.dias@aluno.ifsp.edu.br>
2024-09-16 11:11:19 -0300

    a

--- Math_Language_site/php/criar_turma.php
+++ Math_Language_site/php/criar_turma.php
@@ -6,6 +6,7 @@
         
     $nome_turma = $_POST['nome'];
     $senha_turma = $_POST['senha'];
+    $descricao = $_POST['descricao'];
 
     //CHECAGEM DE POSSIVEIS DADOS JA EXISTENTES//
     $sql = "SELECT nome FROM turma";
@@ -15,7 +16,7 @@
     if()
     {
         //INSERÇÃO DE DADOS//
-        $sql = "INSERT INTO turma(codigo, nome_turma, senha_turma, descricao)";
+        $sql = "INSERT INTO turma(codigo, nome_turma, senha_turma, descricao) VALUES(?, ?, ?, ?)";
 
         $stmt = mysqli_stmt_prepare($link, $sql);
     }
