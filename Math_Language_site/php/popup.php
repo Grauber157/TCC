a0cd359afa068f72cf9976cf38855f0375a328f3
Dinodup <dudup2626@gmail.com>
2024-11-29 22:12:07 -0300

    comita

--- Math_Language_site/php/popup.php
+++ Math_Language_site/php/popup.php
@@ -1,9 +1,10 @@
-<link rel="stylesheet" href="../styles/popup.css">
+<link rel="stylesheet" href="http://localhost:8080/TCC/Math_Language_site/styles/popup.css">
 
 <div id="popup1" class="popup">
     <div class="popup-content">
         <a href="#" class="close">&times;</a> <!-- Link para fechar o pop-up -->
-        <h2>Escolha a dificuldade</h2>
+        <h1>Matemória</h1>
+        <p>Escolha a dificuldade</p>
         <form action="../pages/games/matememoria.php" method="post">
             <button type="submit" name="dificuldade" value="facil">Fácil</button>
             <button type="submit" name="dificuldade" value="medio">Médio</button>
