a5b7e3f352449427e260d450dd86b259b799e130
Gabriel Faria Dias <faria.dias@aluno.ifsp.edu.br>
2024-12-11 17:31:16 -0300

    Sistema de Ranking dos alunos Finalizado (Falta Front-End)

--- Math_Language_site/index.php
+++ Math_Language_site/index.php
@@ -1,12 +1,12 @@
 <?php
     session_start();
-
+    require_once 'core/mysql.php';
+    require_once 'core/sql.php';
+    require_once 'core/conexao_mysql.php';
     if(empty($_SESSION['id']))
     {
       header("Location: pages/login.php");
     }
-    //include("php/include/logout.php");
-    
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -14,10 +14,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/style.css"> 
-    <link rel="stylesheet" href="styles/cabeça.css">
+    <link href="styles/style.css" rel="stylesheet"> 
+    <link href="styles/cabeça.css" rel="stylesheet">
     <link href="styles/fonts.css" rel="stylesheet">
-    <link rel="stylesheet" href="styles/media.css">
+    <link href="styles/media.css" rel="stylesheet">
+    <link href="styles/class.css" rel="stylesheet">
     <link rel="shortcut icon" type="imagex/png" href="Material/images/favicon.ico">
     <title>Math Language</title>
 </head>
@@ -33,11 +34,38 @@
                 Math Language é um Trabalho de Conclusão de Curso com o objetivo de facilitar e reforçar a aprendizagem da matemática básica no Brasil, por meio de mini-jogos interativos, visando o interesse e o incentivo ao conhecimento. Aqui você terá uma jornada extraordinária, se divertindo com nosso conteúdo, explorando o mundo da matemática e ainda assim aprendendo conceitos fundamentais para o nosso dia a dia.
             </p>
         </aside>
-        <article>
-            <img src="#" alt="">
-        </article>
     </main>
 
+
+    <main class="dashboard">
+        <section class="ranking">
+            <h3>Top 10 Alunos</h3>
+                <?php
+                    $sql = "SELECT u.nome_usuario AS nome, SUM(j.pontuacao_jogo) AS pontuacao_total
+                    FROM usuario u
+                    RIGHT JOIN usuario_jogos j ON j.id_usuario = u.id
+                    GROUP BY u.id
+                    ORDER BY pontuacao_total DESC";
+
+                    $turmas = BuscarSql($sql);
+                    $x = 0;
+                    foreach ($turmas as $turma):
+                        if($x < 10)
+                        {
+                            echo '<li><span><strong>'.$turma["nome"].' / '.$turma['pontuacao_total'].'</strong></span></li>';
+                            $x++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    endforeach;
+                ?>
+        </section>
+    </main>
+
+    
     <?php include 'php/include/rodape.php'; ?>
 
 </body>
