57196b2b76e2bf0ea35fb563b3d8c262f4cf7599
Gabriel Faria Dias <faria.dias@aluno.ifsp.edu.br>
2024-12-11 15:01:15 -0300

    Ache o X Sistema de Pontuacao Funcionando

---
+++ Math_Language_site/pages/games/acheox/gameover.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    require_once '../../../php/include/funcoes.php';
+    require_once '../../../core/sql.php';
+    require_once '../../../core/mysql.php';
+    require_once '../../../core/conexao_mysql.php';
+    
+    $pontuacao = $_POST['pontuacao'];
+    Inserir('usuario_jogos', ['id_usuario' => $_SESSION['id'], 'id_jogo' => 4, 'pontuacao_jogo' => $pontuacao]);
+
+    header('Location: ../../games.php');
+    exit;
+?>
